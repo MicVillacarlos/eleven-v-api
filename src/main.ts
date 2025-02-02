1950240248969bec3902055f22bec33d000415aa
MicVillacarlos <micvillacarlos@gmail.com>
2025-02-02 22:02:59 +0800

    install dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConsoleLogger } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  console.log('HEYkk');
-  await app.listen(3000);
+  const logger = new ConsoleLogger('Eleven-V');
+
+  const app = await NestFactory.create(AppModule, {
+    logger: logger,
+  });
+
+  await app.listen(process.env.PORT);
+  logger.log(`** 🚀 Application is running on port ${process.env.PORT} 🚀 **`);
 }
 bootstrap();
