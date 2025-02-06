460b9576a381185f0115e9f8fb9735ec39c1f5de
MicVillacarlos <micvillacarlos@gmail.com>
2025-02-07 01:49:59 +0800

    add schemas

---
+++ src/schemas/users.schema.ts
@@ -0,0 +1,64 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  first_name: string;
+
+  @Prop({ required: true })
+  last_name: string;
+
+  @Prop({ type: Date })
+  birth_date: Date;
+
+  @Prop()
+  sex: string;
+
+  @Prop()
+  home_address: string;
+
+  @Prop()
+  phone_number: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop()
+  emergency_contact_person: string;
+
+  @Prop()
+  emergency_contact_number: string;
+
+  @Prop()
+  occupation: string;
+
+  @Prop()
+  company_or_school: string;
+
+  @Prop()
+  number_of_room_occupants: number;
+
+  @Prop({
+    type: {
+      id: String,
+      room_type: String,
+      price: Number,
+    },
+  })
+  room_details: {
+    id: string;
+    room_type: string;
+    price: number;
+  };
+
+  @Prop()
+  user_type: string;
+
+  @Prop({ default: false })
+  is_verified: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
